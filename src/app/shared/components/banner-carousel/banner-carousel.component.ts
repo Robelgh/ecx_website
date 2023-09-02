4decc49e90b284d357f087aa4e794aefad7290f0
Robelgh <robelghw123@gmail.com>
2023-09-02 17:00:28 +0300

    carousel

--- src/app/shared/components/banner-carousel/banner-carousel.component.ts
+++ src/app/shared/components/banner-carousel/banner-carousel.component.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 import {BannerCarousel} from '../../../_service'
+
+
+interface carouselImage
+{
+  imageSrc:string;
+  imageAlt:string;
+}
 
 @Component({
   selector: 'app-banner-carousel',
   templateUrl: './banner-carousel.component.html',
   styleUrls: ['./banner-carousel.component.css']
 })
-export class BannerCarouselComponent {
+export class BannerCarouselComponent implements OnInit {
 
-  getResponse:any={};
-  bannerCarousel:any=[];
-  imagePath = this.service.getImagePath();
+  @Input() images:carouselImage[] = []
 
-  constructor(private service:BannerCarousel,private router: Router) {}
+  selectedIndex=0;
+
+  ngOnInit(): void {
+
+    console.log(this.images)
+    
+  }
+
+
+
+//   getResponse:any={};
+//   bannerCarousel:any=[];
+//   imagePath = this.service.getImagePath();
+
+//   constructor(private service:BannerCarousel,private router: Router) {}
   
-  async ngOnInit() {
+//   async ngOnInit() {
 
-    this.getResponse = (await this.service.getAll());
-     this.bannerCarousel=this.getResponse.data;
-    console.log(this.bannerCarousel)
+//     this.getResponse = (await this.service.getAll());
+//      this.bannerCarousel=this.getResponse.data;
+//     console.log(this.bannerCarousel)
   
-}
+// }
 
 }
